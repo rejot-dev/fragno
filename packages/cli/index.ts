cd6a8270e52ee91a0fcb635636bb72906b2e2662
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2025-10-02 12:02:38 +0200

    Create: setup interactive cli

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,1 +1,74 @@
-console.log("hey");
+import { defineCommand, runMain } from "citty";
+import { create } from "@fragno-dev/create";
+import * as p from "@clack/prompts";
+
+const main = defineCommand({
+  meta: {
+    name: "create",
+    description: "Interactively create project from template",
+    version: process.env["npm_package_version"],
+  },
+  async run() {
+    p.intro("@fragno-dev/create");
+
+    const name = await p.text({
+      message: "What is your project name?",
+      placeholder: "my-fragment",
+      validate(value) {
+        if (value.length === 0) return "Project name is required!";
+        return undefined;
+      },
+    });
+
+    if (p.isCancel(name)) {
+      p.cancel("Operation cancelled.");
+      process.exit(0);
+    }
+
+    const projectPath = await p.text({
+      message: "Where should we create your project?",
+      placeholder: `./${name}`,
+      initialValue: `./${name}`,
+    });
+
+    if (p.isCancel(projectPath)) {
+      p.cancel("Operation cancelled.");
+      process.exit(0);
+    }
+
+    const template = await p.select({
+      message: "Pick a template",
+      options: [{ value: "fragment", label: "Fragment" }],
+    });
+
+    if (p.isCancel(template)) {
+      p.cancel("Operation cancelled.");
+      process.exit(0);
+    }
+
+    const buildTool = await p.select({
+      message: "Pick a build tool",
+      options: [
+        { value: "tsdown", label: "tsdown" },
+        { value: "esbuild", label: "esbuild" },
+        { value: "none", label: "none" },
+      ],
+    });
+
+    if (p.isCancel(buildTool)) {
+      p.cancel("Operation cancelled.");
+      process.exit(0);
+    }
+
+    create({
+      name: name,
+      path: projectPath,
+      template: template,
+      buildTool: buildTool,
+    });
+
+    p.outro("Project created successfully!");
+  },
+});
+
+runMain(main);
