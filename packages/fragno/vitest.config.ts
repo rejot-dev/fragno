2a95e955d81a310fe567dbd059a4d8df35b7a675
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2025-10-07 15:55:57 +0200

    Include base vitest-config in core

--- packages/fragno/vitest.config.ts
+++ packages/fragno/vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig, mergeConfig } from "vitest/config";
+import { baseConfig } from "@fragno-private/vitest-config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default mergeConfig(
+  baseConfig,
   defineConfig({
     plugins: [svelte(), svelteTesting()],
     test: {
