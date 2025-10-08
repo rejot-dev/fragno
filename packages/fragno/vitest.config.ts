522b055805266ee31689875b26456d17f84ebf3d
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2025-10-08 11:44:18 +0200

    Fix: remove build dependency for tests

--- packages/fragno/vitest.config.ts
+++ packages/fragno/vitest.config.ts
@@ -3,9 +3,8 @@ import { baseConfig } from "@fragno-private/vitest-config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { svelteTesting } from "@testing-library/svelte/vite";
 
-export default mergeConfig(
-  baseConfig,
-  defineConfig({
+export default defineConfig(
+  mergeConfig(baseConfig, {
     plugins: [svelte(), svelteTesting()],
     test: {
       environment: "happy-dom",
