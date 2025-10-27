b62046e9080d28bd74649f82034f184be4080336
Wilco Kruijer <wilcokruijer@gmail.com>
2025-10-27 16:45:37 +0100

    feat(test): add resetDatabase method to test Fragment instance

--- packages/fragno-test/src/index.ts
+++ packages/fragno-test/src/index.ts
@@ -53,6 +53,11 @@ export interface DatabaseFragmentForTest<
   kysely: Kysely<any>;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   adapter: DatabaseAdapter<any>;
+  /**
+   * Resets the database by creating a fresh in-memory database instance and re-running migrations.
+   * After calling this, you should re-initialize any routes to ensure they use the new database instance.
+   */
+  resetDatabase: () => Promise<void>;
 }
 
 export async function createDatabaseFragmentForTest<
@@ -85,19 +90,6 @@ export async function createDatabaseFragmentForTest<
     additionalContext,
   } = options ?? {};
 
-  // Create SQLocalKysely instance
-  const { dialect } = new SQLocalKysely(databasePath);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const kysely = new Kysely<any>({
-    dialect,
-  });
-
-  // Create KyselyAdapter
-  const adapter = new KyselyAdapter({
-    db: kysely,
-    provider: "sqlite",
-  });
-
   // Get schema and namespace from fragment definition's additionalContext
   // Safe cast: DatabaseFragmentBuilder adds databaseSchema and databaseNamespace to additionalContext
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -112,21 +104,42 @@ export async function createDatabaseFragmentForTest<
     );
   }
 
-  // Run migrations automatically
-  const migrator = adapter.createMigrationEngine(schema, namespace);
-  const preparedMigration = migrateToVersion
-    ? await migrator.prepareMigrationTo(migrateToVersion, {
-        updateSettings: false,
-      })
-    : await migrator.prepareMigration({
-        updateSettings: false,
-      });
-  await preparedMigration.execute();
+  // Helper to create a new database instance and run migrations
+  const createDatabase = async () => {
+    // Create SQLocalKysely instance
+    const { dialect } = new SQLocalKysely(databasePath);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const kysely = new Kysely<any>({
+      dialect,
+    });
+
+    // Create KyselyAdapter
+    const adapter = new KyselyAdapter({
+      db: kysely,
+      provider: "sqlite",
+    });
+
+    // Run migrations
+    const migrator = adapter.createMigrationEngine(schema, namespace);
+    const preparedMigration = migrateToVersion
+      ? await migrator.prepareMigrationTo(migrateToVersion, {
+          updateSettings: false,
+        })
+      : await migrator.prepareMigration({
+          updateSettings: false,
+        });
+    await preparedMigration.execute();
+
+    return { kysely, adapter };
+  };
+
+  // Create initial database
+  let { kysely, adapter } = await createDatabase();
 
   // Create fragment with database adapter in options
   // Safe cast: We're merging the user's options with the databaseAdapter, which is required by TOptions
   // The user's TOptions is constrained to FragnoPublicConfig (or a subtype), which we extend with databaseAdapter
-  const mergedOptions = {
+  let mergedOptions = {
     ...fragmentOptions,
     databaseAdapter: adapter,
   } as unknown as TOptions;
@@ -134,7 +147,7 @@ export async function createDatabaseFragmentForTest<
   // Safe cast: If config is not provided, we pass undefined as TConfig.
   // The base createFragmentForTest expects config: TConfig, but if TConfig allows undefined
   // or if the fragment doesn't use config in its dependencies function, this will work correctly.
-  const fragment = createFragmentForTest(fragmentBuilder, {
+  let fragment = createFragmentForTest(fragmentBuilder, {
     config: config as TConfig,
     options: mergedOptions,
     deps,
@@ -142,9 +155,56 @@ export async function createDatabaseFragmentForTest<
     additionalContext,
   });
 
+  // Reset database function - creates a fresh in-memory database and re-runs migrations
+  const resetDatabase = async () => {
+    // Destroy the old Kysely instance
+    await kysely.destroy();
+
+    // Create a new database instance
+    const newDb = await createDatabase();
+    kysely = newDb.kysely;
+    adapter = newDb.adapter;
+
+    // Recreate the fragment with the new adapter
+    mergedOptions = {
+      ...fragmentOptions,
+      databaseAdapter: adapter,
+    } as unknown as TOptions;
+
+    fragment = createFragmentForTest(fragmentBuilder, {
+      config: config as TConfig,
+      options: mergedOptions,
+      deps,
+      services,
+      additionalContext,
+    });
+  };
+
   return {
-    ...fragment,
-    kysely,
-    adapter,
+    get services() {
+      return fragment.services;
+    },
+    get initRoutes() {
+      return fragment.initRoutes;
+    },
+    get handler() {
+      return fragment.handler;
+    },
+    get config() {
+      return fragment.config;
+    },
+    get deps() {
+      return fragment.deps;
+    },
+    get additionalContext() {
+      return fragment.additionalContext;
+    },
+    get kysely() {
+      return kysely;
+    },
+    get adapter() {
+      return adapter;
+    },
+    resetDatabase,
   };
 }
