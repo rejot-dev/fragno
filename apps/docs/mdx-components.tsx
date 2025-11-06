6472b73e1ab23ddcddc578e50cc13eab5ed36b11
Wilco Kruijer <wilcokruijer@gmail.com>
2025-11-06 09:51:56 +0100

    feat(docs): verbosity slider

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -3,6 +3,7 @@ import { AutoTypeTable } from "fumadocs-typescript/ui";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
+import { Levels, Level } from "@/components/levels";
 
 const generator = createGenerator();
 // use this function to get MDX components, you will need it for rendering MDX
@@ -11,6 +12,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...defaultMdxComponents,
     ...TabsComponents,
     ...components,
+    Levels,
+    Level,
     AutoTypeTable: (props) => (
       <AutoTypeTable {...props} generator={generator} options={{ basePath: "../../" }} />
     ),
