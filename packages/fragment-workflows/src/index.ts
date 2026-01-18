cab8f4cec8370325e9eace4057a7b291c922973f
Wilco Kruijer <wilcokruijer@gmail.com>
2026-01-18 13:53:06 +0100

    add workflows runner core

--- packages/fragment-workflows/src/index.ts
+++ packages/fragment-workflows/src/index.ts
@@ -1,3 +1,5 @@
 export { workflowsFragmentDefinition } from "./definition";
+export { workflowsRoutesFactory } from "./routes";
 export { workflowsSchema } from "./schema";
+export { createWorkflowsRunner } from "./runner";
 export * from "./workflow";
