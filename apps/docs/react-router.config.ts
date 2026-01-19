8465cb0163dfcbd9a0c7e48b6a64da1133661f72
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2026-01-19 14:49:44 +0100

    chore: update deps

--- apps/docs/react-router.config.ts
+++ apps/docs/react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: true,
   future: {
-    unstable_viteEnvironmentApi: true,
+    v8_viteEnvironmentApi: true,
     v8_middleware: true,
   },
   // TODO(Wilco): This does not work with Cloudflare's vite plugin
