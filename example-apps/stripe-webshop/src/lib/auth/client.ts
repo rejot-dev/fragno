35c69ee196e10fb13fa8b334e1c09b9cfc1abf4c
Wilco Kruijer <wilcokruijer@gmail.com>
2026-05-18 11:05:24 +0200

    fix(example/stripe): Better Auth typing

--- example-apps/stripe-webshop/src/lib/auth/client.ts
+++ example-apps/stripe-webshop/src/lib/auth/client.ts
@@ -6,7 +6,7 @@ import type { CustomAuth } from "@/lib/auth/auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env["BETTER_AUTH_URL"] || "http://localhost:3000",
-  plugins: [customSessionClient<CustomAuth>(), adminClient()],
+  plugins: [customSessionClient<CustomAuth>(), adminClient({})],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
